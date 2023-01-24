06eef5421ce7927c34b195e7dc0960c26afdf30f
mutonaothenton <mutonaothenton@gmail.com>
2023-01-24 06:02:26 -0500

    scroll y test

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -95,7 +95,7 @@ function removeNavigationEmbed() {
     console.log("Removed iframe #header and #footer");
     changeStyle(); // remove Bootstrap Styles
     document.body.style.overflowX = "hidden"; // to remove iframe scrollbars when scrolling
-    document.body.style.overflowY = "visible"; // to remove iframe scrollbars when scrolling
+    document.body.style.overflowY = "scroll"; // to remove iframe scrollbars when scrolling
   }
   if (self === top) {
     console.log("Top level website so keep native navigation");
